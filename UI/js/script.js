02d86caeace10187aa733a8b8e2d380572c1ed02
Jo_Rdan250 <jordankayinamura@gmail.com>
2019-06-28 17:38:32 -0700

    [feature #166713216] finishes index.html

---
+++ UI/js/script.js
@@ -0,0 +1,22 @@
+function openSlideMenu() {
+    document.getElementById('side-menu').style.width = '200px';
+    document.querySelector('.container').style.marginLEFT = '250px';
+}
+function closeSlideMenu() {
+    document.getElementById('side-menu').style.width = '0';
+}
+
+const report = document.querySelector('.report');
+const popup = document.querySelector('.popup');
+const details = document.querySelector('.bg-modal');
+const img = document.querySelector('.sec2');
+function showModal() {
+    popup.style.display = "block";
+}
+function closeModal() {
+    popup.style.display = "none";
+}
+
+function message() {
+    confirm('Are you sure you want to delete it?');
+}
