c70e01c8581724c5c06e2bcef243844fd171ba0f
Jo_Rdan250 <jordankayinamura@gmail.com>
2019-07-04 20:49:56 -0700

    [feature #167098718] finishes mark as sold endpoint

--- server/routes/property-route.js
+++ server/routes/property-route.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 router.post('/property', property.create);
 router.patch('/property/:id', property.updateProperty);
-// router.patch('/property/:id/sold', property.markSold);
+router.patch('/property/:id/sold', property.markSold);
 router.get('/property', property.allProperties);
 router.get('/property/:id', property.specificProperty);
 // router.delete('/property/:id', property.delete);
