08888ff4c2bad17c1acfd32c78703c02c308edb9
Jo_Rdan250 <jordankayinamura@gmail.com>
2019-07-06 13:47:06 -0700

    [chore #167003668] finishes user validations

--- server/control/user_control.js
+++ server/control/user_control.js
@@ -3,8 +3,9 @@ import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
 import express from "express";
 import bodyParser from "body-parser";
-import register from "../validators/signupValidation";
+import { signupSchema } from '../validators/signUpValidation'
 import { loginSchema } from "../validators/signinValidator";
+import isEmpty from 'lodash.isempty';
 
 const app = express();
 
@@ -21,7 +22,7 @@ class User {
                 message: "User already exist"
             });
         }
-        const newUser = register.validate({
+        const newUser = {
             id: user.length + 1,
             email: req.body.email,
             first_name: req.body.first_name,
@@ -30,16 +31,19 @@ class User {
             phoneNumber: req.body.phoneNumber,
             address: req.body.address,
             isAdmin: false
-        })
-        if (newUser.error !== 'null') {
+        }
+        const { error } = signupSchema(newUser);
+        if (error) {
+            return res.status(400).send({ status: 400, error: error.details[0].message })
+        }
+        else if(!error) {
             const token = jwt.sign({ email: newUser.email }, process.env.secretKey);
             user.push(newUser);
-            console.log(user);
             res.status(201).json({
                 status: "success",
                 data: {
                     token,
-                    newUser: newUser.value
+                    newUser
                 }
             })
         }
@@ -48,27 +52,24 @@ class User {
         }
     }
     static signin(req, res) {
+        if(isEmpty(req.body)){
+            return res.status(400).send({status:'error', message:'Empty fields'});
+        }
         const oneUser = user.find(user => user.email === req.body.email && user.password === req.body.password);
-        if (!oneUser) {
-            return res.status(400).send({ status: 400, error: "invalid user account" })
-        }
         const { error } = loginSchema(req.body);
-        if (error) {
+            if (error) {
             return res.status(400).send({ status: 400, error: error.details[0].message })
-        }
-        const token = jwt.sign({ id: oneUser.id }, process.env.secretkey);
-        if (oneUser) {
+            }
+            if (!oneUser) {
+                return res.status(400).send({ status: 400, error: "invalid user account" })
+            }
+            else {
+            const token = jwt.sign({ id: oneUser.id }, process.env.secretkey);
             res.status(200).send({
                 status: 200,
                 token: token,
                 data: oneUser
             });
-        }
-        else {
-            res.status(400).send({
-                status: 400,
-                message: "Invalid credentials"
-            })
         }
     }
 }
