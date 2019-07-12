25e28d717ebbe6880f499735e19394905b056dc5
Jordan <jordankayinamura@gmail.com>
2019-07-12 15:51:01 -0700

    ch(backend): Prepare readme badges - Setup travis

--- server/app.js
+++ server/app.js
@@ -15,7 +15,7 @@ app.use("/api/v1/auth/", authRouter);
 app.use("/api/v1/", propRouter);
 app.use("/api/v1/", flagRouter);
 
-const PORT = process.env.PORT || 9;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
 });
